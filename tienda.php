44405ed8a8428c56c41d9b44eded490e58d8424c
=Jeferson Vargas <=jeferson.vargas@inacapmail.cl>
2024-04-19 18:55:14 -0400

    Se mejoro la visualizacion de tiendas en vista cliente falta mejorarla

--- tienda.php
+++ tienda.php
@@ -3,142 +3,178 @@
 <?php
 if (isset($_GET['id'])) {
     $id_almacen = urldecode($_GET['id']);
-    echo "Los datos recibidos son: ID: " . $id_almacen . "";
+    //echo "Los datos recibidos son: ID: " . $id_almacen . "";
 }
-
-
+include 'bd.php';
 ?>
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.jsdelivr.net/npm/echarts@5.5.0/dist/echarts.min.js"></script>
-    <link rel="stylesheet" href="css/tienda.css">
-    <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" />
+    <link rel="stylesheet" href="css/tienda.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="css/star.css?v=<?php echo time(); ?>">
+    <title>Datos Almacen</title>
 </head>
 <style>
 
 </style>
 
 <body>
+    <?php
+    $sql1 = "SELECT * FROM tiendas WHERE ID=$id_almacen;";
+    $result1 = mysqli_query($conexion, $sql1);
 
-    <div class="container">
-        <div class="image-container">
-            <img src="https://lh5.googleusercontent.com/p/AF1QipMDXxCPhLZltMZs71Ne621XrEw7NL_sJ0L0GNTD=w750-h606-p-k-no" alt="No Data" srcset="">
+    while ($mostrar2 = mysqli_fetch_array($result1)) {
+    ?>
+        <div class="container">
+            <div class="image-container">
+                <img src="<?php echo $mostrar2['Link_Imagen'] ?>" alt="Sin Imagen">
+            </div>
+            <div class="data-container">
+                <div>
+                    <strong>
+                        <h1><?php echo $mostrar2['Nombre_Almacen'] ?></h1>
+                    </strong>
+                </div>
+                <div>
+
+                    <h2>Direccion:<?php echo $mostrar2['Direccion'] ?></h2>
+
+                </div>
+                <div>
+
+                    <h3>Detalles:<?php echo $mostrar2['Acepta_Tarjetas'] ?> - Checkbox(CajaVecina, Vende Cigarros,etc...)</h3>
+
+                </div>
+            </div>
         </div>
-        <div class="data-container">
-            <div>
-                <strong>
-                    <h1>El Palto</h1>
-                </strong>
-            </div>
-            <div>
-
-                <h2>Direccion: Alcalde Luis Osorio 765, 8150000 Puente Alto, Región Metropolitana</h2>
-
-            </div>
-            <div>
-
-                <h3>Detalles:Checkbox(CajaVecina, Vende Cigarros,etc...)</h3>
-
-            </div>
-        </div>
-    </div>
+    <?php
+    }
+    ?>
     <div class="contenedor">
-        <div id="pan" class="pan div"></div>
+        <div id="pan" class="pan rating-box div"></div>
 
-        <div class="div">
-            <h2>LOS 3 PRODUCTOS MAS VISTOS</h2>
+        <div class="rating-box div">
+            <h2>LOS ULTIMOS 3 PRODUCTOS</h2>
             <table>
-                <tr>
-                    <td>⭐</td>
-                    <td>1</td>
-                    <td>1k Pan</td>
-                    <td>2000</td>
-                </tr>
-                <tr>
-                    <td></td>
-                    <td>2</td>
-                    <td>Mantequilla</td>
-                    <td>2500</td>
+                <?php
+                $sql1 = "SELECT productos.Nombre,(marca.Nombre) as marca,registro_productos.Valor FROM `registro_productos` INNER JOIN productos ON registro_productos.ID_Producto=productos.ID INNER JOIN marca ON productos.ID_Marca=marca.ID where ID_Almacen=$id_almacen ORDER BY registro_productos.ID DESC LIMIT 3;";
+                echo $sql1;
+                $result1 = mysqli_query($conexion, $sql1);
 
-                </tr>
-                <tr>
-                    <td></td>
-                    <td>3</td>
-                    <td>Huevos</td>
-                    <td>300</td>
-
-                </tr>
+                while ($mostrar2 = mysqli_fetch_array($result1)) {
+                ?>
+                    <tr>
+                        <td><?php echo $mostrar2['Nombre'] ?></td>
+                        <td><?php echo $mostrar2['marca'] ?></td>
+                        <td>$<?php echo number_format($mostrar2['Valor'], 0, ',', '.') ?></td>
+                    </tr>
+                <?php
+                }
+                ?>
             </table>
         </div>
-        <div class="div">
-            <h2>CALIFICACION DEL ALMACEN</h2>
-            <h2 style="font-size: 40px;">⭐⭐⭐⭐⭐</h2>
 
-            <h3>Notas: Atencion muy Amable</h3>
+        <div class="rating-box div">
+            <header>Calificacion del Almacen</header>
+            <div class="product-stars-container">
+                <div class="stars product-stars">
+                    <!-- Estrellas del anime -->
+                    <?php
+                    /*
+                $id_anime = $fila["id"];
+
+                $sql = "SELECT promedio FROM calificaciones WHERE ID_Anime=$id_anime"; // Ajusta el ID según tu estructura de base de datos
+                //echo $sql;
+                $result = $conexion->query($sql);
+                
+                // Obtener y almacenar las calificaciones en el array
+                if ($result->num_rows > 0) {
+                    // Obtener la primera fila (solo debería haber una fila si estás buscando un ID específico)
+                    $row = $result->fetch_assoc();
+
+                    $calificacion = $row["promedio"];
+                } else {
+                    $calificacion = 0;
+                }
+                */
+                    $calificacion = 5;
+
+                    // Establecer el número de estrellas activas según la calificación
+                    for ($i = 1; $i <= 5; $i++) {
+                        if ($i <= $calificacion) {
+                            echo '<i class="fa-solid fa-star active"></i>';
+                        } else {
+                            echo '<i class="fa-solid fa-star"></i>';
+                        }
+                    }
+                    ?>
+                </div>
+            </div>
+            <!-- Texto de calificación del anime -->
+            <div class="rating-text product-rating">Promedio: <span class="product-rating-value"><?php echo $calificacion ?></span></div>
         </div>
+
     </div>
 
 
+
+
+    <?php
+
+    $sql1 = "SELECT productos.Nombre,(marca.Nombre) as marca,registro_productos.Valor,registro_promocion.* FROM `registro_productos` INNER JOIN productos ON registro_productos.ID_Producto=productos.ID INNER JOIN marca ON productos.ID_Marca=marca.ID INNER JOIN registro_promocion ON registro_promocion.ID_Registro=registro_productos.ID where ID_Almacen=$id_almacen ORDER BY registro_productos.ID DESC LIMIT 3;";
+
+    $result1 = mysqli_query($conexion, $sql1);
+
+    if ($result1->num_rows > 0) {
+        echo '<h2 style="text-align:center">Promociones</h2>';
+        echo '<div class="data-container  total">';
+        echo "<table";
+        while ($mostrar2 = mysqli_fetch_array($result1)) {
+    ?>
+            <tr>
+                <td><?php echo $mostrar2['Nombre'] ?></td>
+                <td><?php echo $mostrar2['marca'] ?></td>
+                <td>$<?php echo number_format($mostrar2['Valor'], 0, ',', '.') ?></td>
+                <td><?php echo $mostrar2['Promocion'] ?></td>
+                <td><?php
+                    if ($mostrar2['Indefinido'] != "SI") {
+                        echo $mostrar2['Fecha_Termino'];
+                    } else {
+                        echo $mostrar2['Indefinido'];
+                    } ?>
+                </td>
+            </tr>
+    <?php
+        }
+        echo "</table>";
+        echo "</div>";
+    }
+    ?>
+
+    <h2 style="text-align:center">Ultimos Productos</h2>
+
     <div class="data-container total">
         <table>
-            <tr>
-                <th>Producto</th>
-                <th>Valor</th>
-                <th>Promocion</th>
-                <th>Beneficio</th>
-                <th>Fecha_Termino</th>
-            </tr>
-            <tr>
-                <td>Juan</td>
-                <td>30</td>
-                <td>30</td>
-                <td>Madrid</td>
-                <td>Madrid</td>
-            </tr>
-            <tr>
-                <td>María</td>
-                <td>25</td>
-                <td>30</td>
-                <td>Barcelona</td>
-                <td>Madrid</td>
-            </tr>
-            <tr>
-                <td>Carlos</td>
-                <td>40</td>
-                <td>30</td>
-                <td>Valencia</td>
-                <td>Madrid</td>
-            </tr>
-        </table>
-    </div>
-    <div class="data-container total">
-        <table>
-            <tr>
-                <th>Producto</th>
-                <th>Marca</th>
-                <th>Precio</th>
-                <th>Fecha_Registro</th>
-            </tr>
-            <tr>
-                <td>Juan</td>
-                <td>30</td>
-                <td>Madrid</td>
-                <td>Madrid</td>
-            </tr>
-            <tr>
-                <td>María</td>
-                <td>25</td>
-                <td>Barcelona</td>
-                <td>Madrid</td>
-            </tr>
-            <tr>
-                <td>Carlos</td>
-                <td>40</td>
-                <td>Valencia</td>
-                <td>Madrid</td>
-            </tr>
+            <table>
+                <?php
+                $sql1 = "SELECT productos.Nombre,(marca.Nombre) as marca,registro_productos.* FROM `registro_productos` INNER JOIN productos ON registro_productos.ID_Producto=productos.ID INNER JOIN marca ON productos.ID_Marca=marca.ID where ID_Almacen=$id_almacen ORDER BY registro_productos.ID DESC;";
+                $result1 = mysqli_query($conexion, $sql1);
+
+                while ($mostrar2 = mysqli_fetch_array($result1)) {
+                ?>
+                    <tr>
+                        <td><?php echo $mostrar2['Nombre'] ?></td>
+                        <td><?php echo $mostrar2['marca'] ?></td>
+                        <td>$<?php echo number_format($mostrar2['Valor'], 0, ',', '.') ?></td>
+                        <td><?php echo date('Y-m-d', strtotime($mostrar2['Fecha_Registro'])) ?></td>
+                    </tr>
+                <?php
+                }
+                ?>
+            </table>
         </table>
     </div>
 
@@ -146,7 +182,7 @@ if (isset($_GET['id'])) {
     <script type="text/javascript">
         // Initialize the echarts instance based on the prepared dom
         var myChart = echarts.init(document.getElementById('pan'));
-
+        //SELECT * FROM `registro_productos` where ID_Almacen="1" AND ID_Producto="3"
         // Specify the configuration items and data for the chart
         var option = {
             xAxis: {
@@ -194,6 +230,21 @@ if (isset($_GET['id'])) {
 
         // Display the chart using the configuration items and data just specified.
         myChart.setOption(option);
+        // Función para establecer la calificación según el número
+        function setRating(num, stars, ratingText) {
+            const starsList = document.querySelectorAll(stars);
+            const ratingElement = document.querySelector(ratingText);
+
+            starsList.forEach((star, index) => {
+                if (index < num) {
+                    star.classList.add("active");
+                } else {
+                    star.classList.remove("active");
+                }
+            });
+
+            ratingElement.textContent = `${num}`;
+        }
     </script>
 
 </body>
